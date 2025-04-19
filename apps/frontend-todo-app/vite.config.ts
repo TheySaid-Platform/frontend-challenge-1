1e435bf67dd0637c8979e111c79b3492d4d33ee5
hellboy2310 <bhaveshpurohit2310@gmail.com>
2025-04-19 15:47:08 -0400

    changes in the root

--- apps/frontend-todo-app/vite.config.ts
+++ apps/frontend-todo-app/vite.config.ts
@@ -26,5 +26,8 @@ export default defineConfig(() => ({
     commonjsOptions: {
       transformMixedEsModules: true,
     },
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
   },
 }));
