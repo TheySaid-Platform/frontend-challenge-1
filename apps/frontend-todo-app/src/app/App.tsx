a577fc21592b0e5161a5c968485e67db433de78d
hellboy2310 <bhaveshpurohit2310@gmail.com>
2025-04-19 01:22:03 -0400

    feat: intial commit

---
+++ apps/frontend-todo-app/src/app/App.tsx
@@ -0,0 +1,12 @@
+
+export function App() {
+  return(
+    <>
+      Hello world
+    </>
+  )
+}
+
+export default App;
+
+
---
+++ apps/frontend-todo-app/src/recoil/todoAtom.ts
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+
+export const todoListState = atom<string[]>({
+    key:'todoListState',
+    default: [],
+});
