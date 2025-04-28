97c830daff99019c6136122eeac17d76ff7a83d6
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-04-28 05:15:55 +0300

    Create todo interface

--- apps/todo/src/app/components/todo-item.tsx
+++ apps/todo/src/app/components/todo-item.tsx
@@ -1,6 +1,10 @@
-export function TodoItem() {
+import { Todo } from '@todo/interfaces';
+
+export function TodoItem(props: { todo: Todo }) {
+  const { todo } = props;
+  const { id, description, completed } = todo;
   return (
-    <div className="underline">
+    <div className="w-full">
       <h1>Welcome to TodoItem!</h1>
     </div>
   );
