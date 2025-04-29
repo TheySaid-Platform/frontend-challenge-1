7a4390222291e76c03dd2ac957cb027c1a8eedee
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-04-29 18:02:53 +0300

    Persist recoil state

--- libs/recoil-store/src/lib/todo-list.ts
+++ libs/recoil-store/src/lib/todo-list.ts
@@ -1,7 +1,11 @@
 import { atom, SetterOrUpdater } from 'recoil';
 import { Todo } from '@todo/interfaces';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 export const todoListState = atom<Todo[]>({
   key: 'todoListState',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
